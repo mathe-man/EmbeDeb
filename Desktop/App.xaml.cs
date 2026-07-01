b59693a5850de76e8931854077f4d2b1126eae33
mathe-man <mathieumousset6@gmail.com>
2026-07-01 18:33:48 +0300

    Replaced the WinUI project by a WPF project

--- Desktop/App.xaml.cs
+++ Desktop/App.xaml.cs
@@ -1,21 +1,8 @@
-﻿using Microsoft.UI.Xaml;
+﻿using System.Windows;
 
-namespace Desktop
+namespace Desktop;
+
+
+public partial class App : Application
 {
-    public partial class App : Application
-    {
-        private Window? _window;
-
-        // The entry point, equivalent to main
-        public App()
-        {
-            InitializeComponent();
-        }
-
-        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
-        {
-            _window = new MainWindow();
-            _window.Activate();
-        }
-    }
 }
