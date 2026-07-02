bf0a7f667e283798278c85e2fcc4e5a5c7aed5e6
mathe-man <mathieumousset6@gmail.com>
2026-07-02 09:29:28 +0300

    Added a basic MVVM stack for TimeSerie messages

---
+++ Desktop/ViewModels/TimeSerieViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Desktop.Models.Messages;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Desktop.ViewModels;
+
+public partial class TimeSerieViewModel : ObservableObject
+{
+    [ObservableProperty] private string _serieName = string.Empty;
+    [ObservableProperty] private string _serieValues = string.Empty;
+
+    public ObservableCollection<string> AvaibleSeries = new();
+
+    public TimeSerieViewModel()
+    {
+        TimeSerieValueMessage.Handle(new EmbeDebInterpreter.Message.RawMessage("TimeSerieValue", "Serie1,0,1.0"));
+        TimeSerieValueMessage.Handle(new EmbeDebInterpreter.Message.RawMessage("TimeSerieValue", "Serie1,3,4.3"));
+        TimeSerieValueMessage.Handle(new EmbeDebInterpreter.Message.RawMessage("TimeSerieValue", "Serie1,05,-7.1"));
+
+        RefreshSeries();
+    }
+
+    [RelayCommand]
+    public void RefreshSeries()
+    {
+        AvaibleSeries.Clear();
+        foreach (var serie in TimeSerieValueMessage.GetSeriesNames())
+            AvaibleSeries.Add(serie);
+
+        if (_serieName == string.Empty)
+            if (AvaibleSeries.Count > 0)
+                _serieName = AvaibleSeries[0];
+
+
+        _serieValues = string.Empty;
+        foreach (var value in TimeSerieValueMessage.GetSerie(_serieName))
+            _serieValues += $"TimePoint: {value.TimePoint}, Value: {value.Value}\n";
+    }
+
+}
