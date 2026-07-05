ee59f0fe98155c61bef91eebb1de24725ec0a584
mathe-man <mathieumousset6@gmail.com>
2026-07-05 09:37:53 +0200

    Move the Message class to is own file

--- EmbeDebInterpreter/Message/RawMessage.cs
+++ EmbeDebInterpreter/Message/RawMessage.cs
@@ -1,15 +1,6 @@
 ﻿
 namespace EmbeDebInterpreter.Message;
 
-public abstract class Message
-{
-    protected void RaiseObjectCreated()
-        => ObjectCreated?.Invoke(this, EventArgs.Empty);
-
-    public event EventHandler ObjectCreated;
-}
-
-
 public class RawMessage
 {
     public readonly string Type;
