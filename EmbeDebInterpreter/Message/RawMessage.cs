130b65d143e83e8ab5050e2da065a6db9a97fe61
mathe-man <mathieumousset6@gmail.com>
2026-07-21 21:20:32 +0300

    Removed moved class

--- EmbeDebInterpreter/Message/RawMessage.cs
+++ EmbeDebInterpreter/Message/RawMessage.cs
@@ -1,15 +1,6 @@
 ﻿
 namespace EmbeDebInterpreter.Message;
 
-public abstract class Message
-{
-    protected void RaiseObjectCreated()
-        => ObjectCreated?.Invoke(this, EventArgs.Empty);
-
-
-    public static event EventHandler ObjectCreated;
-}
-
 
 public class RawMessage
 {
