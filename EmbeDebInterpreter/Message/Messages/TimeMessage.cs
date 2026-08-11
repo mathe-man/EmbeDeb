0525e3e40f9034c9088bd64cb16a9f4eaf646214
mathe-man <mathieumousset6@gmail.com>
2026-08-11 22:02:09 +0300

    Update the time handling class

---
+++ EmbeDebInterpreter/Message/Messages/TimeMessage.cs
@@ -0,0 +1,33 @@
+﻿
+namespace EmbeDebInterpreter.Message.MessageHandlers;
+
+public class TimeMessage<T> : Message
+{
+    public T Time { get; }
+    public TimeMessage(T time)
+    {
+        Time = time;
+        RaiseObjectCreated();
+    }
+
+    [MessageHandler("time", "time32")]
+    public static Message Handle32(RawMessage me)
+    {
+        var time = UInt32.Parse(me.Content);
+        return new TimeMessage<UInt32>(time);
+    }
+
+    [MessageHandler("time64")]
+    public static Message Handle64(RawMessage me)
+    {
+        var time = UInt64.Parse(me.Content);
+        return new TimeMessage<UInt64>(time);
+    }
+
+    [MessageHandler("time16")]
+    public static Message Handle(RawMessage me)
+    {
+        var time = UInt16.Parse(me.Content);
+        return new TimeMessage<UInt16>(time);
+    }
+}
